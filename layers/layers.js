643fc3173ea243356ba45e18f4e75d476890c31c
kapanadze <kapan.gio777@gmail.com>
2021-08-28 22:21:31 +0400

    version 1
    version 1

--- layers/layers.js
+++ layers/layers.js
@@ -3,18 +3,17 @@ ol.proj.get("EPSG:32638").setExtent([-3177.207962, 4454475.348053, 816270.412000
 var wms_layers = [];
 
 var format_testregionebi_0 = new ol.format.GeoJSON();
-var features_testregionebi_0 = format_testregionebi_0.readFeatures(json_testregionebi_0, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:32638'});
+var features_testregionebi_0 = format_testregionebi_0.readFeatures(json_testregionebi_0, { dataProjection: 'EPSG:4326', featureProjection: 'EPSG:32638' });
 var jsonSource_testregionebi_0 = new ol.source.Vector({
     attributions: ' ',
 });
 jsonSource_testregionebi_0.addFeatures(features_testregionebi_0);
 var lyr_testregionebi_0 = new ol.layer.Vector({
-                declutter: true,
-                source:jsonSource_testregionebi_0, 
-                style: style_testregionebi_0,
-                interactive: true,
-    title: 'test regionebi<br />\
+    declutter: true,
+    source: jsonSource_testregionebi_0,
+    style: style_testregionebi_0,
+    interactive: true,
+    title: 'regionebi<br />\
     <img src="styles/legend/testregionebi_0_0.png" /> <br />\
     <img src="styles/legend/testregionebi_0_1.png" /> Achara<br />\
     <img src="styles/legend/testregionebi_0_2.png" /> Apkhazeti<br />\
@@ -28,13 +27,13 @@ var lyr_testregionebi_0 = new ol.layer.Vector({
     <img src="styles/legend/testregionebi_0_10.png" /> Samtskhe_zhavakheti<br />\
     <img src="styles/legend/testregionebi_0_11.png" /> Shida_kartli<br />\
     <img src="styles/legend/testregionebi_0_12.png" /> Tbilisi<br />'
-        });
+});
 
 lyr_testregionebi_0.setVisible(true);
 var layersList = [lyr_testregionebi_0];
-lyr_testregionebi_0.set('fieldAliases', {'OBJECTID_1': 'OBJECTID_1', 'saxeli': 'saxeli', 'saxeli_en': 'saxeli_en', 'Area_kv_km': 'Area_kv_km', 'Area_ha': 'Area_ha', 'Shape_Leng': 'Shape_Leng', 'Shape_Area': 'Shape_Area', });
-lyr_testregionebi_0.set('fieldImages', {'OBJECTID_1': 'TextEdit', 'saxeli': 'TextEdit', 'saxeli_en': 'TextEdit', 'Area_kv_km': 'TextEdit', 'Area_ha': 'TextEdit', 'Shape_Leng': 'TextEdit', 'Shape_Area': 'TextEdit', });
-lyr_testregionebi_0.set('fieldLabels', {'OBJECTID_1': 'no label', 'saxeli': 'no label', 'saxeli_en': 'no label', 'Area_kv_km': 'no label', 'Area_ha': 'no label', 'Shape_Leng': 'no label', 'Shape_Area': 'no label', });
+lyr_testregionebi_0.set('fieldAliases', { 'saxeli': 'saxeli', 'saxeli_en': 'saxeli_en', 'Area_kv_km': 'Area_kv_km', 'Area_ha': 'Area_ha', 'Shape_Leng': 'Shape_Leng', 'Shape_Area': 'Shape_Area', });
+lyr_testregionebi_0.set('fieldImages', { 'saxeli': 'TextEdit', 'saxeli_en': 'TextEdit', 'Area_kv_km': 'TextEdit', 'Area_ha': 'TextEdit', 'Shape_Leng': 'TextEdit', 'Shape_Area': 'TextEdit', });
+lyr_testregionebi_0.set('fieldLabels', { 'saxeli': 'no label', 'saxeli_en': 'no label', 'Area_kv_km': 'no label', 'Area_ha': 'no label', 'Shape_Leng': 'no label', 'Shape_Area': 'no label', });
 lyr_testregionebi_0.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
